17f174faa89d7169ae021ae25daec63f458dcf31
Nicholas Henry <nicholas.henry@gmail.com>
2021-08-11 09:29:13 -0400

    Remove generated boilerplate

--- lib/message_db.ex
+++ lib/message_db.ex
@@ -1,18 +1,2 @@
 defmodule MessageDb do
-  @moduledoc """
-  Documentation for `MessageDb`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MessageDb.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
